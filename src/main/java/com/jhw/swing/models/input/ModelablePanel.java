01965386b215cba642f8fd6638f95d2ce6266f8f
Unknown <unknown@example.com>
2020-08-08 20:16:16 -0500

    ModelablePanel: +class. Movida la interfaz al proyecto

---
+++ src/main/java/com/jhw/swing/models/input/ModelablePanel.java
@@ -0,0 +1,27 @@
+package com.jhw.swing.models.input;
+
+/**
+ * Interfaz a implementar por los paneles que van a trabajar con la creacion y
+ * edicion de modelos, incluye metodos basicos para su trabajo.
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * @param <T> Tipo de Modelo que va a trabajar.
+ */
+public interface ModelablePanel<T> {
+
+    public T getNewModel();
+
+    public T getOldModel();
+
+    public void setOldModel(T model);
+
+    public T onDeleteAction();
+
+    public T onCreateAction();
+
+    public boolean onCancelAction();
+
+    public T onPostCreateAction(T obj);
+
+    public T onPostDeleteAction(T obj);
+}
