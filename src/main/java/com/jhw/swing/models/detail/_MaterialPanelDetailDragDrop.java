7334d8faa8cd19c387a4da895ec7fda4f3caf048
Unknown <unknown@example.com>
2020-09-02 00:00:25 -0500

    +_MaterialPanelDetailDragDrop

---
+++ src/main/java/com/jhw/swing/models/detail/_MaterialPanelDetailDragDrop.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.swing.models.detail;
+
+import com.clean.core.app.services.ExceptionHandler;
+import com.clean.core.domain.DomainObject;
+import com.jhw.excel.utils.DomainListFileReader;
+import com.jhw.swing.material.components.table.Column;
+import com.jhw.swing.material.standards.MaterialIcons;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * @param <T>
+ */
+public abstract class _MaterialPanelDetailDragDrop<T extends DomainObject> extends _MaterialPanelDetail<T> {
+
+    private final DomainListFileReader<T> reader;
+
+    public _MaterialPanelDetailDragDrop(DomainListFileReader<T> reader) {
+        this(reader, new Column[]{});
+    }
+
+    public _MaterialPanelDetailDragDrop(DomainListFileReader<T> reader, Column... arr) {
+        super(arr);
+        this.reader = reader;
+        personalize();
+    }
+
+    public void setButtonAddTransferConsumer(DomainListFileReader<T> reader) {
+        header.getButtonAdd().setButtonAddTransferConsumer((List<File> t) -> {
+            try {
+                insertAll(reader.read(t));
+            } catch (Exception e) {
+                ExceptionHandler.handleException(e);
+            }
+        });
+    }
+
+    public void insertAll(List<T> newDomains) {
+        for (T newDomain : newDomains) {
+        }
+    }
+
+    public Object[] getRowObjectExport(T object) {
+        return getRowObject(object);
+    }
+
+    public String[] getColumnNamesExport() {
+        return getColumnNames();
+    }
+
+    private void personalize() {
+        this.addOptionElement(new AbstractAction("Exportar a Excel", MaterialIcons.EXTENSION) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onExportToExcelAction();
+            }
+        });
+
+        setButtonAddTransferConsumer(reader);
+    }
+
+    private void onExportToExcelAction() {
+        JOptionPane.showMessageDialog(null, Arrays.asList(getColumnNamesExport()));
+        for (T t : list) {
+            JOptionPane.showMessageDialog(null, t);
+        }
+
+    }
+}
