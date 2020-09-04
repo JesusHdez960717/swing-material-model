da370a41a94504bf635da862014ad1e31b60d090
Unknown <unknown@example.com>
2020-09-03 21:51:54 -0500

    _MaterialPanelDetailDragDrop: + popupMenu. Varios export

--- src/main/java/com/jhw/swing/models/detail/_MaterialPanelDetailDragDrop.java
+++ src/main/java/com/jhw/swing/models/detail/_MaterialPanelDetailDragDrop.java
@@ -11,17 +11,25 @@ import com.jhw.excel.utils.DomainListFileReader;
 import com.jhw.excel.utils.ExcelListWriter;
 import com.jhw.files.utils.PersonalizationFiles;
 import com.jhw.personalization.services.PersonalizationHandler;
+import com.jhw.swing.material.components.button._MaterialButtonIconTransparent;
 import com.jhw.swing.material.components.filechooser.FileDropHandler;
 import com.jhw.swing.material.components.table.Column;
 import com.jhw.swing.material.standards.MaterialIcons;
+import com.jhw.swing.material.standards.MaterialImages;
 import com.jhw.swing.models.utils.PersonalizationModel;
+import com.jhw.swing.util.AbstractActionUtils;
 import static com.jhw.utils.others.SDF.SDF_ALL;
 import com.jhw.utils.services.ConverterService;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JTable;
 
 /**
@@ -71,23 +79,14 @@ public abstract class _MaterialPanelDetailDragDrop<T extends DomainObject> exten
     }
 
     private void personalize() {
-        this.addOptionElement(new AbstractAction("Exportar a Excel", MaterialIcons.EXCEL.deriveIcon(24f)) {
+        Action excelAction = new AbstractAction("Exportar (Excel por defecto). Click derecho para desplegar TODAS las opciones de exportación.", MaterialIcons.EXCEL.deriveIcon(24f)) {
             @Override
             public void actionPerformed(ActionEvent e) {
                 onExportToExcelAction();
             }
-        });
-        /*this.addOptionElement(new AbstractAction("123", MaterialIcons.PRINT) {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    getJTable().print(JTable.PrintMode.FIT_WIDTH);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });*/
-        
+        };
+        excelAction.putValue("popup", getPopupAllExportSupported());
+        addOptionElement(excelAction);
         if (reader != null) {
             setButtonAddTransferConsumer(reader);
         }
@@ -115,5 +114,49 @@ public abstract class _MaterialPanelDetailDragDrop<T extends DomainObject> exten
         } catch (Exception e) {
             ExceptionHandler.handleException(e);
         }
+    }
+
+    private void onExportToExcelSelectedAction() {
+        try {
+            ExcelListWriter.builder b = exportBuilder()
+                    .folder(new File(PersonalizationHandler.getString(PersonalizationFiles.KEY_TEMP_FOLDER)))
+                    .fileName(header.getHeaderText())
+                    .setColumns(this::getColumnNamesExport)
+                    .values(ConverterService.convert(getSelectedList(), this::getRowObjectExport));
+
+            //personaliza con las cosas que quiera
+            personalizeBuilder(b);
+
+            b.write().open();
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e);
+        }
+    }
+
+    private JPopupMenu getPopupAllExportSupported() {
+        JPopupMenu menu = new JPopupMenu("Exportar a:");
+
+        //crea el menu de excel
+        JMenuItem menuExcel = new JMenu(AbstractActionUtils.from("Excel", MaterialIcons.EXCEL));
+        //accion de exportarlo todo a excel
+        JMenuItem excelAll = new JMenuItem(new AbstractAction("Exportar Todo", MaterialIcons.SELECT_ALL) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onExportToExcelAction();
+            }
+        });
+        menuExcel.add(excelAll);
+        //accion de exportar lo seleccionado a excel
+        JMenuItem excelSelected = new JMenuItem(new AbstractAction("Exportar Seleccionado", MaterialIcons.FLIP) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onExportToExcelSelectedAction();
+            }
+        });
+        menuExcel.add(excelSelected);
+        //agregado el menu de excel al menu general
+        menu.add(menuExcel);
+
+        return menu;
     }
 }
