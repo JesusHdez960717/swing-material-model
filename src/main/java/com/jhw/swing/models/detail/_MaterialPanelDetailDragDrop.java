7369494c9cf4cad35db4b1e3c12a0cfb876b7441
Unknown <unknown@example.com>
2020-09-03 19:19:54 -0500

    _MaterialPanelDetailDragDrop: +personalizeBuilder, +_MaterialPanelDetailDragDrop(Column... arr)

--- src/main/java/com/jhw/swing/models/detail/_MaterialPanelDetailDragDrop.java
+++ src/main/java/com/jhw/swing/models/detail/_MaterialPanelDetailDragDrop.java
@@ -9,9 +9,12 @@ import com.clean.core.app.services.ExceptionHandler;
 import com.clean.core.domain.DomainObject;
 import com.jhw.excel.utils.DomainListFileReader;
 import com.jhw.excel.utils.ExcelListWriter;
+import com.jhw.files.utils.PersonalizationFiles;
+import com.jhw.personalization.services.PersonalizationHandler;
 import com.jhw.swing.material.components.filechooser.FileDropHandler;
 import com.jhw.swing.material.components.table.Column;
 import com.jhw.swing.material.standards.MaterialIcons;
+import com.jhw.swing.models.utils.PersonalizationModel;
 import static com.jhw.utils.others.SDF.SDF_ALL;
 import com.jhw.utils.services.ConverterService;
 import java.awt.event.ActionEvent;
@@ -19,6 +22,7 @@ import java.io.File;
 import java.util.Date;
 import java.util.List;
 import javax.swing.AbstractAction;
+import javax.swing.JTable;
 
 /**
  *
@@ -37,6 +41,10 @@ public abstract class _MaterialPanelDetailDragDrop<T extends DomainObject> exten
         super(arr);
         this.reader = reader;
         personalize();
+    }
+
+    public _MaterialPanelDetailDragDrop(Column... arr) {
+        this(null, arr);
     }
 
     public void setButtonAddTransferConsumer(DomainListFileReader<T> reader) {
@@ -69,20 +77,41 @@ public abstract class _MaterialPanelDetailDragDrop<T extends DomainObject> exten
                 onExportToExcelAction();
             }
         });
-
-        setButtonAddTransferConsumer(reader);
+        /*this.addOptionElement(new AbstractAction("123", MaterialIcons.PRINT) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    getJTable().print(JTable.PrintMode.FIT_WIDTH);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });*/
+        
+        if (reader != null) {
+            setButtonAddTransferConsumer(reader);
+        }
     }
 
-    public ExcelListWriter.builder exportBuilder() {
+    protected ExcelListWriter.builder exportBuilder() {
         return ExcelListWriter.builder();
+    }
+
+    protected void personalizeBuilder(ExcelListWriter.builder builder) {
     }
 
     private void onExportToExcelAction() {
         try {
-            exportBuilder().fileName(header.getHeaderText() + " " + SDF_ALL.format(new Date()))
+            ExcelListWriter.builder b = exportBuilder()
+                    .folder(new File(PersonalizationHandler.getString(PersonalizationFiles.KEY_TEMP_FOLDER)))
+                    .fileName(header.getHeaderText())
                     .setColumns(this::getColumnNamesExport)
-                    .values(ConverterService.convert(list, this::getRowObjectExport))
-                    .write().open();
+                    .values(ConverterService.convert(list, this::getRowObjectExport));
+
+            //personaliza con las cosas que quiera
+            personalizeBuilder(b);
+
+            b.write().open();
         } catch (Exception e) {
             ExceptionHandler.handleException(e);
         }
