f0302f0c5aa7bf6f2589d2656462fdbbc822dca9
Unknown <unknown@example.com>
2020-09-22 00:25:00 -0500

    +CleanDetailCRUDDragDrop

---
+++ src/main/java/com/jhw/swing/models/clean/CleanDetailCRUDDragDrop.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.swing.models.clean;
+
+import com.clean.core.app.services.ExceptionHandler;
+import com.clean.core.domain.DomainObject;
+import com.jhw.excel.utils.DomainListFileReader;
+import com.jhw.swing.material.components.table.Column;
+import com.jhw.swing.models.detail._MaterialPanelDetailDragDrop;
+import com.jhw.swing.models.input.dialogs.DialogModelInput;
+import com.jhw.swing.models.input.panels.ModelPanel;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public abstract class CleanDetailCRUDDragDrop<T extends DomainObject> extends _MaterialPanelDetailDragDrop<T> implements PropertyChangeListener {
+
+    public CleanDetailCRUDDragDrop(DomainListFileReader<T> reader) {
+        super(reader);
+        addPropertyChange();
+        personalize();
+    }
+
+    public CleanDetailCRUDDragDrop(Column... arr) {
+        super(arr);
+        addPropertyChange();
+        personalize();
+    }
+
+    public CleanDetailCRUDDragDrop(DomainListFileReader<T> reader, Column... arr) {
+        super(reader, arr);
+        addPropertyChange();
+        personalize();
+    }
+
+    protected abstract void personalize();
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        super.propertyChange(evt);
+        switch (evt.getPropertyName()) {
+            case "create":
+                update();
+                break;
+            case "edit":
+                update();
+                break;
+            case "destroy":
+                update();
+                break;
+            case "destroyById":
+                update();
+                break;
+        }
+    }
+
+    protected abstract void addPropertyChange();
+
+    @Override
+    public void update() {
+        try {
+            setCollection(getListUpdate());
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e);
+        }
+    }
+
+    protected abstract List<T> getListUpdate() throws Exception;
+
+    @Override
+    protected void buttonNuevoActionListener() {
+        DialogModelInput.from(getModelPanelNew());
+    }
+
+    protected abstract ModelPanel<T> getModelPanelNew();
+
+    @Override
+    protected void editAction(T obj) {
+        DialogModelInput.from(getModelPanelEdit(obj));
+    }
+
+    protected abstract ModelPanel<T> getModelPanelEdit(T obj);
+}
